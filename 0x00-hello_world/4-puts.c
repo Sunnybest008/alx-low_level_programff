f150f95bd2b0d04d5c0d26ba5950c2c8fb163f73
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-10 09:19:25 -0700

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@ int main(void)
  * Return: Always 0 (Success)
  */
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+       	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 

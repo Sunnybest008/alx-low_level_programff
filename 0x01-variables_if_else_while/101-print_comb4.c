c1f939354660302537679ad2a7c2357d90ff2aa2
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-13 15:13:09 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description:' prints all possible different combinations of three digits'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int o;
+
+		for (n  = '0'; n  < '9'; n++)
+		{
+		for (m = n + 1; m <= '9'; m++)
+		{
+		for (o = m + 1; o <= '9'; o++)
+		{
+
+			if ((m != n) != o)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(o);
+				if (n == '7' && m == '8')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		}
+		}
+		putchar('\n');
+		return (0);
+}

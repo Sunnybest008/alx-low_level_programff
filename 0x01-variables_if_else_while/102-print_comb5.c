7e8a988525fa6085664019b76f84933afd28b79e
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-13 16:01:54 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,14 +16,14 @@ int main(void)
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
 			putchar(' ');
-				putchar((b / 10) + '0');
-				putchar((b % 10) + '0');
-				if (a == 98 && b == 99)
-			continue;
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n');
-	return (0);
+			putchar('\n');
+			return (0);
 }

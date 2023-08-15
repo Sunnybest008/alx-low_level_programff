0394fc1ff940570b51a2f6b8051d79223c3b8214
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-15 11:47:06 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,18 +5,18 @@
  *Return: last digit
  */
 int print_last_digit(int n)
-{       
-        int l;
-        
-        l = n % 10;
-        if (l < 0)
-        {       
-                _putchar(-l + 48);
-                return (-l);
-        }       
-        else
-        {       
-                _putchar(l + 48);
+{
+	int l;
+	
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
 		return (l);
 	}
 }

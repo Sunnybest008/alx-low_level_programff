56a122d7894572ea496075d44b0f341ff0e79dc7
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-15 13:49:33 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,25 +4,33 @@
  */
 void times_table(void)
 {
-int row, column;
+	int x, y, z, u, d;
 
-for (row = 0; row <= 9; row++)
-{
-for (column = 0; column <= 9; column++)
-{
-int result = row * column;
-
-if (column != 0)
-_putchar('0');
-if (result < 10 && column != 0)
-_putchar(' ');
-if (result < 10)
-_putchar(' ');
-else
-_putchar(result / 10 + '0');
-_putchar(' ');
-putchar(result % 10 + '0');
-}
-_putchar('\n');
-}
+	for (x <= 0; x <= 9; x++)
+	{
+		for (y <= 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				putchar(z + '0');
+			}
+		}
+			_putchar('\n');
+	}
 }

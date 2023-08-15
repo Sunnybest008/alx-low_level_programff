ace6ae43f4cc90a1e682fcc2d7d1c85a8fd7a98d
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-15 15:19:10 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,7 +24,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n <= 98; n--)
+		for (; n >= 98; n--)
 		{
 			if (n == 98)
 			{
@@ -38,5 +38,6 @@ void print_to_98(int n)
 			}
 		}
 	}
+	printf("\n");
 }
 

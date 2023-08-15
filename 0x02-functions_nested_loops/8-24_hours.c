87e490d133bfb70416c227a8bb13daa9e4972166
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-15 12:57:02 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,12 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 b <= 3))
-			{
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
+					if (a >= 2 && b >= a)
+						break;
 					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(58);
@@ -26,7 +26,7 @@ void jack_bauer(void)
 					_putchar('\n');
 				}
 			}
-			}
+		
 		}
 	}
 }

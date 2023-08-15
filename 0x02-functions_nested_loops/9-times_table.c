8ea287a5ddddf5401d76f9c929d367bb5ddee33a
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-15 13:58:24 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,9 @@ void times_table(void)
 {
 	int x, y, z, u, d;
 
-	for (x <= 0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (y <= 0; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
 			if (z > 9)
@@ -28,7 +28,7 @@ void times_table(void)
 					_putchar(32);
 					_putchar(32);
 				}
-				putchar(z + '0');
+				_putchar(z + '0');
 			}
 		}
 			_putchar('\n');

894c61652e8e9991c2cba187492bb05b50a00702
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-23 10:45:34 -0700

    0x06-pointers_arrays_strings# git commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,25 @@
 #include "main.h"
-
 /**
- *strncpy - a function that copies a string.
+ *_strncpy - a function that copies a string
  *@dest: destination string pointer.
  *@src: source string pointer.
- *@n: number of bytes to be copied.
- *Return: pointer to destination string.
+ *@n: number or bytes to be used
+ *Return: pointer to destination string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int count_of_bytes;
-	for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
+
+	count_of_bytes = 0;
+	while (count_of_bytes < n && src[count_of_bytes] != '\0')
+	{
 		dest[count_of_bytes] = src[count_of_bytes];
-	for (; count_of_bytes < n; count_of_bytes++)
+		count_of_bytes++;
+	}
+	while (count_of_bytes < n)
+	{
 		dest[count_of_bytes] = '\0';
+		count_of_bytes++;
+	}
 	return (dest);
 }

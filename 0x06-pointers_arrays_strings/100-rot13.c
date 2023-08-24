6b03a2bcc1ca51474ffe1576c5935589e2af7b6c
Sunnybest008 <bestsuuny6@gmail.com>
2023-08-24 05:24:57 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,24 @@
 #include "main.h"
-
+/**
+ * rot13 - Encodes a string using the ROT13 substitution cipher.
+ * @s: The input string to be encoded.
+ *
+ * This function takes a string as input
+ * and applies the ROT13 substitution
+ * cipher to each character in the string.
+ * ROT13 replaces each letter with
+ * the letter 13 places down the alphabet.
+ * Non-letter characters remain
+ * unchanged.
+ *
+ * The function iterates through the input
+ * string and replaces each letter
+ * with its corresponding ROT13 counterpart.
+ * It leaves non-letter characters
+ * untouched. The modified string is then returned.
+ *
+ * Return: A pointer to the modified input string.
+ */
 char *rot13(char *s)
 {
 	char *ptr = s;
@@ -9,6 +28,7 @@ char *rot13(char *s)
 	while (*ptr != '\0')
 	{
 		int i = 0;
+
 		while (original[i] != '\0')
 		{
 			if (*ptr == original[i])
@@ -21,5 +41,5 @@ char *rot13(char *s)
 		ptr++;
 	}
 
-	return s;
+	return (s);
 }
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,25 @@
 #include "main.h"
-
+/**
+ * print_number - Prints an integer to the standard output.
+ * @n: The integer to be printed.
+ *
+ * This function takes an integer as input
+ * and prints it to the standard
+ * output. It handles both positive and negative integers.
+ *
+ * The function first checks if the integer is negative.
+ * If it is, it prints
+ * a minus sign and converts the integer to its
+ * positive counterpart for printing.
+ *
+ * Then, the function recursively divides
+ * the integer by 10 and prints each
+ * digit in reverse order. This process
+ * effectively prints the integer as a
+ * sequence of characters.
+ *
+ * Return: None (void).
+ */
 void print_number(int n)
 {
 	if (n < 0)
